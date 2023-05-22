9e9b502e438f0f74eb5f45ff1c66710fb1258579
Vikram <vikramsingh001@gmail.com>
2023-05-22 21:37:12 +0200

    Add single page to app

---
+++ src/SinglePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SinglePage = () => {
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>Some text goes here.</p>
+        </div>
+    );
+};
+
+export default SinglePage;
