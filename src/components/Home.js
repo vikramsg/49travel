86e69422132e4a320ea8b99e1d8077224aa6d152
Vikram <vikramsingh001@gmail.com>
2023-06-05 23:21:09 +0200

    Test no link for other cities

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,31 +13,26 @@ const Home = () => {
         {
             title: "Berlin",
             text: "Coming soon!",
-            link: "/origin/berlin",
             clickable: false,
         },
         {
             title: "Munich",
             text: "Coming soon!",
-            link: "/origin/munich",
             clickable: false,
         },
         {
             title: "Frankfurt",
             text: "Coming soon!",
-            link: "/origin/frankfurt",
             clickable: false,
         },
         {
             title: "Cologne",
             text: "Coming soon!",
-            link: "/origin/cologne",
             clickable: false,
         },
         {
             title: "Stuttgart",
             text: "Coming soon!",
-            link: "/origin/stuttgart",
             clickable: false,
         }
     ];
@@ -47,7 +42,10 @@ const Home = () => {
             <Row xs={1} md={2} lg={2} className="g-4">
                 {cards.map((card, index) => (
                     <Col key={index}>
-                        <Link to={card.link} className="text-decoration-none">
+                        <Link
+                            to={card.title === 'Hamburg' ? card.link : '#'}
+                            className={`text-decoration-none ${card.title === 'Hamburg' ? 'card-clickable' : ''}`}
+                        >
                             <Card className={`h-100 ${card.clickable ? 'card-clickable' : ''}`}>
                                 <Card.Body>
                                     <Card.Title>{card.title}</Card.Title>
