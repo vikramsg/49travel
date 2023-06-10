d121d74f8f0324f91703aedbe21fde447a95046f
Vikram <vikramsingh001@gmail.com>
2023-06-10 21:02:25 +0200

    Try again

--- src/App.js
+++ src/App.js
@@ -19,10 +19,6 @@ const App = () => {
           {/* Add more routes for other pages */}
         </Routes>
       </Router>
-      <script
-        async src="https://analytics.umami.is/script.js"
-        data-website-id="59eb34b7-ef67-48e8-a55c-9d387ab2661a">
-      </script>
       <Analytics />;
     </div>
   );
